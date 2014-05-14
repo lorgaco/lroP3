5ea7ebc5c0d70d9706e2190b450ec6f36c3a5b2a
lorgaco <lorenzo.gaco@gmail.com>
2014-05-14 17:14:43 +0200

    Tvml errors in first page.

--- TvmlReader.java
+++ TvmlReader.java
@@ -88,6 +88,7 @@ public class TvmlReader {
                                     daysList.add(date);
                                 }
                                 else {
+                                    if(errors.equals("All files ok")) errors = "";
                                     errors = errors + Error + "<br /><br />";
                                 }
                             } catch (Exception ex) {
@@ -95,7 +96,7 @@ public class TvmlReader {
                                 final StringWriter sw = new StringWriter();
                                 final PrintWriter pw = new PrintWriter(sw, true);
                                 ex.printStackTrace(pw);
-                                errors = errors + "Error: " + ex.toString() + "<br /><br />";
+                                errors = errors + "Error: " + ex.toString() + "<br />";
                             }
                         }
                     }
